335a6b2c206e36945f7f168cfb26cb351b15411f
Amulya Musipatla <amusipatla@fb.com>
2017-07-13 12:39:56 -0700

    fixed path

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("serviceAccountKey.json");
+var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
